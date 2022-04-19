6277892d351db4db2247eb0d77e3e046598d4cb1
inemurikozoo <100814669+inemurikozoo@users.noreply.github.com>
2022-04-19 21:00:26 +0000

    rails app making

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,28 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to　book_path(book_id)
+  end
+
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+  
+  def destroy
+  end
+  
+  
+  private
+  #ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
