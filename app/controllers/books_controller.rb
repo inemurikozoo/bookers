8f465fac2d6fb31542bc296167d6f2d40b86215e
inemurikozoo <100814669+inemurikozoo@users.noreply.github.com>
2022-04-23 04:40:40 +0000

    destroy finished.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,14 +24,19 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id), notice: "Book was successflly updated."
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "Book was successflly updated."
+    else
+      render :edit
+    end
   end  
   
   
   def destroy
-    
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to request.referer, notice: "Book was successflly destroyed."
   end
   
   
