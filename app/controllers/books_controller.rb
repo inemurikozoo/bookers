78283f5d35a48b5b4471850d82415e9c77c0aae4
inemurikozoo <100814669+inemurikozoo@users.noreply.github.com>
2022-04-23 10:33:12 +0000

    bookers make finish.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(book.id), notice: "Book was successflly created."
+      redirect_to book_path(@book.id), notice: "Book was successflly created."
     else
       @books = Book.all
       render :index
@@ -26,7 +26,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book), notice: "Book was successflly updated."
+      redirect_to book_path(@book.id), notice: "Book was successflly updated."
     else
       render :edit
     end
